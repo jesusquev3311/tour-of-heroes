f6b30e550cea2674c9c4a800c5146a12e6ab88cb
jesus quevedo <jesus@gokapital.com>
2018-11-26 17:34:37 -0500

    http requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,32 @@ import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessageComponent } from './message/message.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+    InMemoryDataService, { dataEncapsulation: false }
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,27 +8,19 @@ import { HeroService } from '../hero.service'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'WindStorm'
-  };
 
   heroes : Hero[];
 
-  selectedHero: Hero;
+  
+  constructor(private heroService: HeroService) { }
 
-  onSelect(hero: Hero):void {
-    this.selectedHero = hero;
+  ngOnInit() {
+    this.getHeroes();
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
-  }
-  constructor(private heroService: HeroService) { }
-
-  ngOnInit() {
-    this.getHeroes();
   }
 
 }
